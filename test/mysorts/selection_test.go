22cf0364358c5b3623634071a50948c0a115bc21
jamison.andaluz@gmail.com <jamison.andaluz@gmail.com>
2019-01-20 14:14:09 -0500

    adding test for selection sort

---
+++ test/mysorts/selection_test.go
@@ -0,0 +1,52 @@
+package mysorts
+
+import (
+	"fmt"
+	"mysorts"
+	"sort"
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	copyIntList := make([]int, len(intList))
+	copy(copyIntList, intList)
+	copyIntList = mysorts.SelecitonSort(copyIntList)
+	sorted := sort.SliceIsSorted(copyIntList, func(i, j int) bool {
+		fmt.Println(i, j)
+		fmt.Println("vals:", copyIntList[i], copyIntList[j])
+		return copyIntList[i] < copyIntList[j]
+	})
+
+	if !sorted {
+		t.Errorf("List was not sorted: %v", copyIntList)
+	}
+	copyIntList = make([]int, len(intList2))
+	copy(copyIntList, intList2)
+	fmt.Println(copyIntList)
+	copyIntList = mysorts.SelecitonSort(copyIntList)
+	sorted = sort.SliceIsSorted(copyIntList, func(i, j int) bool {
+		if j < len(copyIntList) {
+			return copyIntList[i] < copyIntList[j]
+		}
+		return true
+	})
+	if !sorted {
+		t.Errorf("List was not sorted: %v", copyIntList)
+	}
+
+	copyIntList = make([]int, len(intList3))
+	copy(copyIntList, intList3)
+	fmt.Println("Before: ", copyIntList)
+	copyIntList = mysorts.SelecitonSort(copyIntList)
+	fmt.Println("After: ", copyIntList)
+	sorted = sort.SliceIsSorted(copyIntList, func(i, j int) bool {
+		if j < len(copyIntList) {
+			return copyIntList[i] < copyIntList[j]
+		}
+		return true
+	})
+	if !sorted {
+		t.Errorf("List was not sorted: %v", copyIntList)
+	}
+
+}
